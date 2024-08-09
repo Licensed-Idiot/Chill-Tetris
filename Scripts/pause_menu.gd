878366df1a66cc9c026b6bf7f76ad284f3d2e90c
LicensedIdiot <170021962+Licensed-Idiot@users.noreply.github.com>
2024-08-09 16:54:28 -0400

    Added Options Menu, add functionality next

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,21 +1,34 @@
 extends Control
 
+@onready var options_menu = $OptionsMenu as OptionsMenu
+@onready var game_over_menu = $"../GameOverMenu" as GameOver
+@onready var panel_container = $PanelContainer as PanelContainer
+
 var _is_paused: bool = false:
 	set = show_pause_menu
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
+		if panel_container.visible == false:
+			panel_container.visible = true
+			options_menu.visible = false
 		_is_paused = !_is_paused
 
 func show_pause_menu(value: bool) -> void:
-	_is_paused = value
-	get_tree().paused = _is_paused
-	visible = _is_paused
-
+	if game_over_menu.visible == true:
+		pass
+	else:
+		_is_paused = value
+		get_tree().paused = _is_paused
+		visible = _is_paused
 
 func _on_resume_button_pressed():
 	_is_paused = false
 
+func _on_options_pressed():
+	panel_container.visible = false
+	options_menu.set_process(true)
+	options_menu.visible = true
 
 func _on_restart_button_pressed():
 	_is_paused = false
@@ -25,3 +38,8 @@ func _on_restart_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_options_menu_exit_options_menu():
+	panel_container.visible = true
+	options_menu.visible = false
--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -2,7 +2,9 @@ extends CanvasLayer
 
 class_name UI
 
-@onready var game_over_menu = $GameOverMenu
+@onready var game_over_menu = $GameOverMenu as GameOver
+@onready var panel_container = $GameOverMenu/PanelContainer
+@onready var options_menu = $GameOverMenu/OptionsMenu as OptionsMenu
 
 func show_game_over():
 	game_over_menu.show()
@@ -12,6 +14,15 @@ func _on_button_pressed():
 	get_tree().reload_current_scene()
 	BackroundMusic.play()
 
+func _on_options_button_pressed():
+	panel_container.visible = false
+	options_menu.set_process(true)
+	options_menu.visible = true
+
+func _on_options_menu_exit_options_menu():
+	panel_container.visible = true
+	options_menu.visible = false
 
 func _on_quit_pressed():
 	get_tree().quit()
+
